f0d4d08a05932723865d53c4bf98fd007b0a7c96
James Moore <moorejames099@gmail.com>
2015-08-14 19:33:25 -0600

    Initial commit

---
+++ User.py
@@ -0,0 +1,32 @@
+class User():
+
+	def __init__(self):
+		self._name = ""
+		self._pass = ""
+		self._cmd = None
+		self._recipies = []
+		self.
+
+	@staticMethod
+	def createUser(self):
+		type = input("Would you like to be an author (a) or a consumer (c)? ")
+		newUser = None
+		if(type == "a"):
+			newUser = AuthorUser()
+		else:
+			newUser = ConsumerUser()
+		newUser._name = input("Please enter a username: ")
+		newUser._password = input("Please enter a password: ")
+		return newUser
+
+	def checkLogin(self, name, password):
+		if(self._name == name):
+			if(self._pass == password):
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	def run(self):
+		
---
+++ main.py
@@ -0,0 +1,31 @@
+import cmd
+
+class CmdPrompt(cmd.Cmd):
+	"""Simple command processor"""
+
+	def do_register(self, line):
+		newUser = User.create()
+		newUser.run()
+
+	def do_login(self, line):
+		print("logging in")
+
+	def do_load(self, line):
+		print("loading file")
+
+	def do_storage(self, line):
+		print("setting storage file")
+
+	def do_exit(self, line):
+		return True
+
+class CmdPrompt2(cmd.Cmd):
+
+	def do_exit(self, line):
+		return True
+
+	def do_print(self, line):
+		print("hi from 2")
+
+if __name__ == '__main__':
+	CmdPrompt().cmdloop()
